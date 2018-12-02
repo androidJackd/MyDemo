b1b5c8b918e021e83394c620374751a3d798f5b6
androidJackd <1025505572@qq.com>
2018-12-02 23:02:16 +0800

    save code

--- app/src/main/java/com/example/administrator/mydemo/wight/FlowLayout.java
+++ app/src/main/java/com/example/administrator/mydemo/wight/FlowLayout.java
@@ -1,4 +1,24 @@
 package com.example.administrator.mydemo.wight;
 
-public class FlowLayout {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class FlowLayout extends ViewGroup{
+    public FlowLayout(Context context) {
+        super(context);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
 }
